4ec5447c7adf1cc013a6cbad99c5f099ae290a22
Matthew Kane <mkb-prime@hydrogenproject.com>
2010-02-28 20:23:42 -0500

    getting ready to merge this branch back into master

--- src/evolver/evolvertest.cpp
+++ src/evolver/evolvertest.cpp
@@ -16,9 +16,9 @@ using namespace nynex;
 int main(int argc, char **argv) {
     SampleBank & bank = SampleBank::getInstance();
     bank.setChannels(2);
-    bank.setSampleRate(48000);
+    bank.setSampleRate(44100);
     bank.setSampleSize(2);
-    bank.setSampleDir("/Users/makane/code/nynex/samples");
+    bank.setSampleDir("/Users/makane/code/nynex/subset");
     Composition c;
     bank.initComposition(c);
     c.bounceToFile("/Users/makane/code/nynex/output/whatwhat.mp3");
