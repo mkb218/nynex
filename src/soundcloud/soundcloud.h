b37d6d5f84e803023de4d826c9b010ff706d0b02
Matthew Kane <mkb-prime@hydrogenproject.com>
2010-03-08 20:53:53 -0500

    new tasks. new xcode version. some progress on soundcloud wrapper. more projects in about page.

--- src/soundcloud/soundcloud.h
+++ src/soundcloud/soundcloud.h
@@ -17,12 +17,19 @@
 namespace nynex {
     class SoundCloudServer {
     public:
-        void fetchDropBox(); // uses samplebank singleton
+        static SoundCloudServer StagingServerFactory(std::string host, std::string username, std::string password) { return SoundCloudServer(host, username, password); }
+        static SoundCloudServer ProdServerFactory(std::string host, std::string apiKey) { return SoundCloudServer(host, apiKey); }
+        void fetchDropBox(); // uses samplebank singleton and afconvert to add samples
         std::vector<std::string> submitCompositions(const std::vector<boost::reference_wrapper<Composition> > &) const; // returns list of identifiers used for streaming
     private:
+        SoundCloudServer(std::string host, std::string username, std::string password) : host_(host), username_(username), password_(password), useKey_(false) {}
+        SoundCloudServer(std::string host, std::string apiKey) : host_(host), apiKey_(apiKey), useKey_(true) {}
         std::string host_;
+        std::string username_;
+        std::string password_;
         std::string apiKey_;
-    };
+        bool useKey_;
+    }; 
 }
 
 #endif
