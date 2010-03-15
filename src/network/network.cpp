b99afc425032dae392ee5948a445b76f5af15566
Matthew Kane <mkb-prime@hydrogenproject.com>
2010-03-15 18:45:43 -0400

    network functions using libcurl

--- src/network/network.cpp
+++ src/network/network.cpp
@@ -1,5 +1,96 @@
 #include "network.h"
+#include <libkern/OSAtomic.h>
+#include "composition.h"
 
 using namespace nynex;
 
-CurlContext * CurlContext::context_ = NULL;
+volatile CurlContext *CurlContext::context_ = NULL;
+volatile bool CurlContext::init_ = false;
+
+volatile CurlContext * CurlContext::getContext() {
+    if (!OSAtomicTestAndSet(7, &CurlContext::init_)) {
+        context_ = new CurlContext();
+    }
+    return context_; 
+}
+
+void getUrl(const std::string & url, const std::string & user = "", const std::string & password = "") {
+    char errorbuf[CURL_ERROR_SIZE];
+    volatile CurlContext *context = CurlContext::getContext();
+    CURL * curl = curl_easy_init();
+    if (curl == NULL) throw std::runtime_error("curl easy init failed!");
+    try {
+        CURLcode result = curl_easy_setopt(curl, CURLOPT_URL, url.c_str());
+        if (result != CURLE_OK) {
+            throw std::runtime_error("setopt URL failed: " + stringFrom(result));
+        }
+        
+        if (user.size() > 0 && password.size() > 0) {
+            result = curl_easy_setopt(curl, CURLOPT_USERNAME, user.c_str());
+            if (result != CURLE_OK) {
+                throw std::runtime_error("setopt USER failed: " + stringFrom(result));
+            }
+
+            result = curl_easy_setopt(curl, CURLOPT_PASSWORD, user.c_str());
+            if (result != CURLE_OK) {
+                throw std::runtime_error("setopt PASS failed: " + stringFrom(result));
+            }
+        }
+        
+        result = curl_easy_perform(curl);
+        if (result != CURLE_OK) {
+            throw std::runtime_error("setopt PERFORM failed: " + stringFrom(result) + " " + errorbuf);
+        }
+        
+    } catch (...) {
+        curl_easy_cleanup(curl);
+        throw;
+    }
+    curl_easy_cleanup(curl);
+}
+
+
+void postUrl(const std::string & url, const std::string & body, const std::string & user = "", const std::string & password = "") {
+    char errorbuf[CURL_ERROR_SIZE];
+    volatile CurlContext *context = CurlContext::getContext();
+    CURL * curl = curl_easy_init();
+    if (curl == NULL) throw std::runtime_error("curl easy init failed!");
+    try {
+        CURLcode result = curl_easy_setopt(curl, CURLOPT_URL, url.c_str());
+        if (result != CURLE_OK) {
+            throw std::runtime_error("setopt URL failed: " + stringFrom(result));
+        }
+
+        result = curl_easy_setopt(curl, CURLOPT_POST, 1);
+        if (result != CURLE_OK) {
+            throw std::runtime_error("setopt POST failed: " + stringFrom(result));
+        }
+        
+        result = curl_easy_setopt(curl, CURLOPT_POSTFIELDS, body.c_str());
+        if (result != CURLE_OK) {
+            throw std::runtime_error("setopt POSTFIELDS failed: " + stringFrom(result));
+        }
+        
+        if (user.size() > 0 && password.size() > 0) {
+            result = curl_easy_setopt(curl, CURLOPT_USERNAME, user.c_str());
+            if (result != CURLE_OK) {
+                throw std::runtime_error("setopt USER failed: " + stringFrom(result));
+            }
+            
+            result = curl_easy_setopt(curl, CURLOPT_PASSWORD, user.c_str());
+            if (result != CURLE_OK) {
+                throw std::runtime_error("setopt PASS failed: " + stringFrom(result));
+            }
+        }
+        
+        result = curl_easy_perform(curl);
+        if (result != CURLE_OK) {
+            throw std::runtime_error("setopt PERFORM failed: " + stringFrom(result) + " " + errorbuf);
+        }
+        
+    } catch (...) {
+        curl_easy_cleanup(curl);
+        throw;
+    }
+    curl_easy_cleanup(curl);
+}
--- src/network/network.h
+++ src/network/network.h
@@ -2,19 +2,21 @@
 #ifndef NYNEX_NETWORK
 #define NYNEX_NETWORK
 
-#include <list>
+#include <curl/curl.h>
+#include <stdexcept>
 
 namespace nynex {  
     class CurlContext {
     public:
-        static CurlContext *getContext() { if (context_ == NULL) context = new CurlContext(); return context_; }
+        static volatile CurlContext *getContext();
         ~CurlContext() { curl_global_cleanup(); }
     private:
-        CurlContext() { curl_global_init(); }
-        static CurlContext *context_;
+        CurlContext() { if (0 != curl_global_init(CURL_GLOBAL_NOTHING)) throw std::runtime_error("curl system init failed: "); }
+        static volatile CurlContext *context_;
+        static volatile bool init_;
     };
     
     void getUrl(const std::string & url);
-    void postUrl(const std::string & url, const std::string & body, CFHTTPAuthenticationRef auth = NULL);
+    void postUrl(const std::string & url, const std::string & body, const std::string & user = "", const std::string & password = "");
 }
 #endif
