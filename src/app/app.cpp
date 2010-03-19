c1501546577b106665b7b9230b269f5c824a3c35
Matthew Kane <mkb-prime@hydrogenproject.com>
2010-03-19 09:28:14 -0400

    more app work

--- src/app/app.cpp
+++ src/app/app.cpp
@@ -1,18 +1,82 @@
 #include "app.h"
 
+using namespace nynex;
+
+// app has three modes, passed in sequence
+// 0.) init ga, bounce all comps to files and play
+// 1.) we play all compositions in sequence, timer starts
+// 2.) display buttons for each composition allowing users to listen and rate. display running countdown
+// 3.) time's up, retrieve web ratings. if there are ratings, step ga and bounce, otherwise just go back to mode 1
+
+
+
 //--------------------------------------------------------------
 void nynexApp::setup(){
-
+    // fullscreen
+    ofSetFullscreen(true);
+    
+    // set background color
+    ofSetFrameRate(60);
+    
+    // set frame rate + vert refresh
+    // init ga (from file if exists)
+    // create soundcloud and twitter servers from settings file, google voice downloader, web rating downloader
+    // add notifiers to ga
 }
 
 //--------------------------------------------------------------
-void nynexApp::update(){
-
+void nynexApp::update() {
+    switch (mode_) {
+        case STARTUP:
+            bounceComps();
+            startPlayComp(); // will call resetTimer()
+            mode_ = GENERATION_START;
+            resetFrames();
+            break;
+        case GENERATION_START:
+            if (!player.getIsPlaying()) {
+                if (moreComps()) {
+                    playNextComp();
+                } else {
+                    mode_ = GENERATION_WAIT;
+                    resetFrames();
+                }
+            }
+            break;
+        case GENERATION_WAIT:
+            if (timesUp()) {
+                if (gotRatings()) {
+                    evolver_->stepGA();
+                    bounceComps();
+                }
+                startPlayComp();
+                resetFrames();
+                mode_ = GENERATION_START;
+            }
+            break;
+        default:
+            mode_ = GENERATION_START; // can't happen!
+            resetFrames();
+    }
 }
 
 //--------------------------------------------------------------
 void nynexApp::draw(){
-
+    switch (mode_) {
+        case STARTUP:
+            drawStartup();
+            break;
+        case GENERATION_START:
+            drawGenStart();
+            break;
+        case GENERATION_END:
+            drawGenEnd();
+            break;
+        default:
+            // can't happen
+            mode_ = GENERATION_START;
+    }
+    ++framesSinceStateChange_;
 }
 
 //--------------------------------------------------------------
@@ -32,7 +96,7 @@ void nynexApp::mouseMoved(int x, int y ){
 
 //--------------------------------------------------------------
 void nynexApp::mouseDragged(int x, int y, int button){
-
+    // 
 }
 
 //--------------------------------------------------------------
--- src/app/app.h
+++ src/app/app.h
@@ -1,24 +1,92 @@
 #ifndef _NYNEX_APP
 #define _NYNEX_APP
 
-
 #include "ofMain.h"
+#include "evolver.h"
+#include "soundcloud.h"
+#include "twitter.h"
 
+#define POPSIZE 10
+#define GEN_LIMIT_MILLIS 3600 * 1000
+#define RATINGS 5
+#define RATEBUTTON_RADIUS 100
+#define PLAYBUTTON_RADIUS 100
+#define ACTIVE_RADIUS_INCREMENT 50
+
+#define TRANSITION_FRAMES 60
+#define FONTFILE_PATH "/Library/Fonts/Futura.dfont"
+
+#define LR_MARGIN 100
+#define VERT_MARGIN 100
+
+#define START_R 0
+#define END_R 255
+#define R_SLOPE ((END_R - START_R) / (float)POPSIZE)
+#define START_G 0
+#define END_G 255
+#define G_SLOPE ((END_G - START_G) / (float)POPSIZE)
+#define START_B 255
+#define END_B 0
+#define B_SLOPE ((END_B - START_B) / (float)POPSIZE)
+
+namespace nynex {
 class nynexApp : public ofBaseApp{
 
-	public:
-		void setup();
-		void update();
-		void draw();
+public:
+    nynexApp() : mode_(STARTUP), sc_(NULL), evolver_(NULL), twitter_(NULL) {
+        ofBaseApp();
+    }
+    ~nynexApp() { 
+        delete sc_;
+        delete evolver_;
+        delete twitter_;
+    }
+    void setup();
+    void update();
+    void draw();
 
-		void keyPressed  (int key);
-		void keyReleased(int key);
-		void mouseMoved(int x, int y );
-		void mouseDragged(int x, int y, int button);
-		void mousePressed(int x, int y, int button);
-		void mouseReleased(int x, int y, int button);
-		void windowResized(int w, int h);
-
+    void keyPressed  (int key);
+    void keyReleased(int key);
+    void mouseMoved(int x, int y );
+    void mouseDragged(int x, int y, int button);
+    void mousePressed(int x, int y, int button);
+    void mouseReleased(int x, int y, int button);
+    void windowResized(int w, int h);
+private:
+    enum Mode { STARTUP,
+        GENERATION_START,
+        GENERATION_WAIT } mode_;
+    struct Button {
+        float x;
+        float y;
+        struct {
+            int r;
+            int g;
+            int b;
+        } color;
+        float radius;
+    }
+        
+    void resetTimer() { timer_ = ofGetElapsedTimeMillis(); }
+    bool timesUp() { return (ofGetElapsedTimeMillis() - timer_) > GEN_LIMIT_MILLIS; }
+    void resetFrames() { framesSinceStateChange_ = 0; }
+    void bounceComps();
+    void startPlayComp();
+    void playNextComp();
+    bool gotRatings();
+    bool moreComps();
+    void drawStartup();
+    void drawGenEnd();
+    void drawGenStart();
+    int timer_;
+    unsigned int framesSinceStateChange_;
+    Evolver * evolver_;
+    SoundCloudServer * sc_;
+    TwitterServer * twitter_;
+    ofSoundPlayer player;
+    size_t compIndex_;
+    Button[POPSIZE] playButtons;
+    Button[RATINGS] rateButtons;
 };
-
+}
 #endif
