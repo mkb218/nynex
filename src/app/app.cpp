da172087efab3a1b613439da0c925174ea5c86ed
Matthew Kane <mkb-prime@hydrogenproject.com>
2010-03-19 18:08:16 -0400

    more app stuff

--- src/app/app.cpp
+++ src/app/app.cpp
@@ -2,11 +2,14 @@
 
 using namespace nynex;
 
-// app has three modes, passed in sequence
+// app has following states
 // 0.) init ga, bounce all comps to files and play
-// 1.) we play all compositions in sequence, timer starts
-// 2.) display buttons for each composition allowing users to listen and rate. display running countdown
-// 3.) time's up, retrieve web ratings. if there are ratings, step ga and bounce, otherwise just go back to mode 1
+// 1.) we play all compositions in sequence, timer starts. once playing is done, commence step 2. display running countdown.
+// 2.) display buttons for each composition allowing users to listen. display running countdown
+// 3.) if user clicks listen button, display rating buttons plus cancel button. rating times out after 5 minutes
+// 4a.) user clicks rating button, rating stored, back to step 2
+// 4b.) user clicks cancel button, no rating stored, back to step 2
+// 5.) time's up, retrieve web ratings. if there are ratings, step ga and bounce, otherwise just go back to step 1
 
 
 
@@ -16,47 +19,49 @@ void nynexApp::setup(){
     ofSetFullscreen(true);
     
     // set background color
+    ofSetBackground(BG_R, BG_G, BG_B);
     ofSetFrameRate(60);
     
     // set frame rate + vert refresh
     // init ga (from file if exists)
     // create soundcloud and twitter servers from settings file, google voice downloader, web rating downloader
     // add notifiers to ga
+    bounceComps();
+    startPlayComp(); // will call resetTimer()
+    switchState(GENERATION_START);
 }
 
 //--------------------------------------------------------------
 void nynexApp::update() {
     switch (mode_) {
-        case STARTUP:
-            bounceComps();
-            startPlayComp(); // will call resetTimer()
-            mode_ = GENERATION_START;
-            resetFrames();
-            break;
         case GENERATION_START:
             if (!player.getIsPlaying()) {
                 if (moreComps()) {
                     playNextComp();
                 } else {
-                    mode_ = GENERATION_WAIT;
-                    resetFrames();
+                    switchState(GENERATION_LIST);
                 }
             }
             break;
-        case GENERATION_WAIT:
+        case GENERATION_LIST:
             if (timesUp()) {
                 if (gotRatings()) {
                     evolver_->stepGA();
                     bounceComps();
                 }
                 startPlayComp();
-                resetFrames();
-                mode_ = GENERATION_START;
+                switchState(GENERATION_START);
             }
             break;
+        case GENERATION_RATE:
+            if (timesUp()) {
+                switchState(GENERATION_LIST);
+            }
+            break;
+        case GENERATION_END:
+            break;
         default:
-            mode_ = GENERATION_START; // can't happen!
-            resetFrames();
+            switchState(GENERATION_START); // can't happen!
     }
 }
 
@@ -96,21 +101,23 @@ void nynexApp::mouseMoved(int x, int y ){
 
 //--------------------------------------------------------------
 void nynexApp::mouseDragged(int x, int y, int button){
-    // 
+    // if button == left mouse
+    // if position within a button, change activebutton
 }
 
 //--------------------------------------------------------------
 void nynexApp::mousePressed(int x, int y, int button){
-
+    // if button == left mouse
+    // if position within a button, change activebutton
 }
 
 //--------------------------------------------------------------
 void nynexApp::mouseReleased(int x, int y, int button){
-
+    // check position against all buttons
 }
 
 //--------------------------------------------------------------
 void nynexApp::windowResized(int w, int h){
-
+    ofSetFullscreen(true); // don't fuck with the window, jerkface
 }
 
--- src/app/app.h
+++ src/app/app.h
@@ -8,10 +8,12 @@
 
 #define POPSIZE 10
 #define GEN_LIMIT_MILLIS 3600 * 1000
+#define RATE_LIMIT_MILLIS 60 * 1000 * 5
 #define RATINGS 5
 #define RATEBUTTON_RADIUS 100
 #define PLAYBUTTON_RADIUS 100
 #define ACTIVE_RADIUS_INCREMENT 50
+#define HOVER_RADIUS_INCREMENT 50
 
 #define TRANSITION_FRAMES 60
 #define FONTFILE_PATH "/Library/Fonts/Futura.dfont"
@@ -19,13 +21,16 @@
 #define LR_MARGIN 100
 #define VERT_MARGIN 100
 
+#define BG_R 255
+#define BG_G 255
+#define BG_B 255
 #define START_R 0
-#define END_R 255
+#define END_R 128
 #define R_SLOPE ((END_R - START_R) / (float)POPSIZE)
-#define START_G 0
-#define END_G 255
+#define START_G 64
+#define END_G 0
 #define G_SLOPE ((END_G - START_G) / (float)POPSIZE)
-#define START_B 255
+#define START_B 0
 #define END_B 0
 #define B_SLOPE ((END_B - START_B) / (float)POPSIZE)
 
@@ -33,7 +38,7 @@ namespace nynex {
 class nynexApp : public ofBaseApp{
 
 public:
-    nynexApp() : mode_(STARTUP), sc_(NULL), evolver_(NULL), twitter_(NULL) {
+    nynexApp() : mode_(STARTUP), sc_(NULL), evolver_(NULL), twitter_(NULL), activeButton_(NULL) {
         ofBaseApp();
     }
     ~nynexApp() { 
@@ -53,9 +58,11 @@ public:
     void mouseReleased(int x, int y, int button);
     void windowResized(int w, int h);
 private:
-    enum Mode { STARTUP,
-        GENERATION_START,
-        GENERATION_WAIT } mode_;
+    enum Mode { GENERATION_START,
+        GENERATION_LIST,
+        GENERATION_RATE,
+        GENERATION_END
+    } mode_;
     struct Button {
         float x;
         float y;
@@ -69,7 +76,7 @@ private:
         
     void resetTimer() { timer_ = ofGetElapsedTimeMillis(); }
     bool timesUp() { return (ofGetElapsedTimeMillis() - timer_) > GEN_LIMIT_MILLIS; }
-    void resetFrames() { framesSinceStateChange_ = 0; }
+    void switchState(Mode state) { mode_ = state; framesSinceStateChange_ = 0; }
     void bounceComps();
     void startPlayComp();
     void playNextComp();
@@ -85,8 +92,9 @@ private:
     TwitterServer * twitter_;
     ofSoundPlayer player;
     size_t compIndex_;
-    Button[POPSIZE] playButtons;
-    Button[RATINGS] rateButtons;
+    Button *activeButton_;
+    Button[POPSIZE] playButtons_;
+    Button[RATINGS] rateButtons_;
 };
 }
 #endif
