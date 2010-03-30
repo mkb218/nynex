eea3905f38aacf812a90b155d456c4823093f95f
Matthew Kane <mkb-prime@hydrogenproject.com>
2010-03-30 08:48:39 -0400

    images fixed up

--- www/static/rate.js
+++ www/static/rate.js
@@ -1,7 +1,7 @@
 soundManager.flashVersion = 9;
 soundManager.debugMode = false; // disable debug mode
 soundManager.defaultOptions.multiShot = false;
-soundManager.url = 'soundcloud-javascript-player'; // change to '.' to use local non-crossdomain soundmanager
+soundManager.url = '.'; // change to '.' to use local non-crossdomain soundmanager
 $(function() {
   $("a.soundcloud-player#basic").scPlayer();
 });
--- www/static/rate.php
+++ www/static/rate.php
@@ -6,8 +6,18 @@
 	$timeleft = fgets($file);
 	$timeleft = $timeleft - time();
 	$seconds = $timeleft % 60;
+	if ($seconds == 1) {
+		$seconds = "1 second";
+	} else {
+		$seconds = "$seconds seconds";
+	}
 	$minutes = intval($timeleft / 60);
-	$timeleft = "$minutes minutes and $seconds seconds";
+	if ($minutes == 1) {
+		$minutes = "1 minute";
+	} else {
+		$minutes = "$minutes minutes";
+	}
+	$timeleft = "$minutes and $seconds";
 	while (!feof($file)) {
 		$line = fgets($file);
 		$kv = explode("=", $line, 2);
@@ -31,7 +41,7 @@
 		<div id="pagetitle">The Republic of Nynex - rating generation <?php echo $generation; ?></div>
 		<div id="pagesubtitle">Please listen to and rate the following compositions. This generation has <?php echo $timeleft; ?> remaining.</div>
 <?php foreach ($tracks as $i => $url) { ?>
-		<div id="content">Individual <?php echo $i ?>: <a class="soundcloud-player" id="basic" href="<?php echo $url; ?>">Play</a><?php foreach (array(1,2,3,4,5) as $vote) { ?><a href="" name="rate<?php echo $i; ?>-<?php echo $vote; ?>"><img src="rate<?php echo $vote; ?>.png" /></a> <?php } ?><br/></div>
+		<div id="content">Individual <?php echo $i ?>: <?php foreach (array(1,2,3,4,5) as $vote) { ?><a href="" name="rate<?php echo $i; ?>-<?php echo $vote; ?>"><img src="rate<?php echo $vote; ?>.png" alt="Rate <?php echo $vote; ?> points for individual <?php echo $i ?>" height="16"/></a> <?php } ?><a class="soundcloud-player" id="basic" href="<?php echo $url; ?>">Play</a><br/><?php if ($i != 0) { ?><hr/><?php } ?></div>
 <?php } ?>
 		<div id="content">From here, you can <a href="http://github.com/mkb218/nynex/">follow the software's development</a>, <a href="blosxom.cgi">read the progress blog</a>, <a href="http://twitter.com/nynexrepublic">follow the project on Twitter</a>, <a href="live.html">find out where in-person demonstrations will be occurring</a>, <a href="http://soundcloud.com/republic-of-nynex">listen to the project's creations on SoundCloud</a>, <a href="http://mail231.csoft.net/mailman/listinfo/nynex-announce">join the announcement mailing list</a>, <a href="about.html">read more detailed information about the project</a>, <a href="http://www.facebook.com/pages/Republic-of-Nynex/268718961023">become a Facebook fan</a>, or <a href="contrib.html">see information on audio contributors</a>. Soon, you will be able contribute your own audio recordings, listen to previous generations of compositions, and read about the Republic of Nynex's creator.</div>
 		<div id="cc"><p>This site looks best if you have the Futura typeface installed.</p><a href="http://www.twitter.com/nynexrepublic"><img src="http://twitter-badges.s3.amazonaws.com/twitter-c.png" alt="Follow nynexrepublic on Twitter"/></a><a rel="license" href="http://creativecommons.org/licenses/by-nc-sa/3.0/us/"><img alt="Creative Commons License" style="border-width:0" src="http://i.creativecommons.org/l/by-nc-sa/3.0/us/88x31.png" /></a><br />Audio output of this project is licensed under a <a rel="license" href="http://creativecommons.org/licenses/by-nc-sa/3.0/us/">Creative Commons Attribution-Noncommercial-Share Alike 3.0 United States License</a>.<br/>&copy; 2010 <a href="http://hydrogenproject.com">Matthew Kane</a></div>
