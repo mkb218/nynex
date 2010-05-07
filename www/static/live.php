4053917d534666b748309115e0aa4bb0d0366fd4
Matthew Kane <mkb-prime@hydrogenproject.com>
2010-05-07 14:56:49 -0400

    adding installation data

--- www/static/live.php
+++ www/static/live.php
@@ -4,7 +4,7 @@
 	$title = "Demonstrations";
 	$subtitle = "The Republic of Nynex is designed to serve as a bridge between the online world and the physical one.";
 	$content = <<<EOT
-<p>The Republic of Nynex will be part of the 2010 <A href="http://megapolisfestival.org">Megapolis Festival</a> in Baltimore, from May 14 to May 16. More information will be available soon.</p>
+<p>The Republic of Nynex will be part of the 2010 <A href="http://megapolisfestival.org">Megapolis Festival</a> in Baltimore, from May 14 to 16. The installation will be in the lobby of WYPR 88.1 FM, at 2216 North Charles St., in Baltimore.</p>
 EOT;
 	require("template.inc");
 ?>
