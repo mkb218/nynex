dd7fb9053bcf02026048c99a52de46b24e8042a7
Matthew Kane <mkb-prime@hydrogenproject.com>
2010-05-12 17:21:50 -0400

    reorg about page

--- www/static/about.php
+++ www/static/about.php
@@ -5,6 +5,8 @@
 	$subtitle = "The Republic of Nynex is an audio art installation that uses genetic programming to create audio compositions using audience-submitted raw audio.";
 	$content = <<<EOT
 	<p>The Republic of Nynex was created in response to a call for submissions to the 2010 <a href="http://megapolisfestival.org/">Megapolis Festival</a> in Baltimore. The name derives from NYNEX, the company formed by the merger of Baby Bells New England Telephone and New York Telephone. The Republic of Nynex as a fictional state could be thought of as the northern segment of the independent BosWash in some dystopian post-US future. Or, it could be something else.</p>
+	<p><a href="http://hydrogenproject.com">Matthew Kane</a>, the creator, is a software engineer, amateur musician, and occasional college radio DJ living in Salem, Massachusetts.</p>
+        <hr />
 	<p>The Republic of Nynex is written primarily in C++, taking advantage of numerous open source projects and web services.</p>
 	<p><a href="http://lancet.mit.edu/ga/">GALib</a> is an MIT-funded project implementing genetic algorithms, allowing developers to take advantage of genetic programming research without starting from scratch.</p>
 	<p><a href="http://hydrogenproject.com/pages/fs1rgen.html">FS1rgen</a> is another GAlib-based project used as the starting point for the Republic of Nynex.</p>
@@ -21,8 +23,6 @@
 	<p><a href="http://twitter.com">Twitter</a>... well, you probably all know what Twitter is. Republic of Nynex uses Twitter to announce new generations.</p>
 	<p><a href="http://liboauth.sourceforge.net">liboauth</a> is a software package implementing Twitter's OAuth API, used to connect to both SoundCloud.</a></p>
 	<p><a href="http://gnu.ethz.ch/linuks.mine.nu/beteckna/">Beteckna</a> is a GPL-licensed font inspired by Futura.</p>
-	<hr />
-	<p>The Republic of Nynex was created by <a href="http://hydrogenproject.com">Matthew Kane</a>, a software engineer and college radio DJ from Salem, Massachusetts.</p>
 EOT;
 	require("template.inc");
 ?>
