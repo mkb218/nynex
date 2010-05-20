8abd684c1161912fa9feb6385d76fb3030823dda
Matthew Kane <mkb-prime@hydrogenproject.com>
2010-05-20 10:00:12 -0400

    live updates

--- www/static/live.php
+++ www/static/live.php
@@ -4,7 +4,8 @@
 	$title = "Demonstrations";
 	$subtitle = "The Republic of Nynex is designed to serve as a bridge between the online world and the physical one.";
 	$content = <<<EOT
-<p>The Republic of Nynex will be part of the 2010 <A href="http://megapolisfestival.org">Megapolis Festival</a> in Baltimore, from May 14 to 16. The installation will be in the lobby of WYPR 88.1 FM, at 2216 North Charles St., in Baltimore.</p>
+<p>There are no future live events confirmed. To schedule a live demonstration, the easiest way to contact me is through <a href="http://twitter.com/nynexrepublic">Twitter</a>.
+<p>The Republic of Nynex was a part of the 2010 <A href="http://megapolisfestival.org">Megapolis Festival</a> in Baltimore, from May 14 to 16. The installation was set up in the lobby of WYPR 88.1 FM. Pictures forthcoming.</p>
 EOT;
 	require("template.inc");
 ?>
